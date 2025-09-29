9fd878ff2b9f9ac9e9bcc0b557d100313b424968
Ganesh <ganeshchary4001@gmail.com>
2025-09-29 14:36:30 +0530

    Update and rename outpots.tf to provider.tf

--- EKS-TF/outpots.tf
+++
@@ -1,8 +0,0 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS cluster"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
-}
