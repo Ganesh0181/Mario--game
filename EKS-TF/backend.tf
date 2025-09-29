9455952a08b989d74903f3fa662bf90d5426047e
Ganesh <ganeshchary4001@gmail.com>
2025-09-29 15:26:42 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my_terraform_state_bucket"
-    key    = "eks/terraform.tfstate"
-    region = "us-west-2"
+    bucket = "ganesh-mrcloudbook777"
+    key    = "EKS/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
 
